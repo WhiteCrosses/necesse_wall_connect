bf59582f842b51d2aff836c91de5dcce36726181
WhiteCrosses <szymon.modrzynski@gmail.com>
2025-12-21 20:19:41 +0100

    Comments for clarifying problems

---
+++ src/main/java/ExampleMod.java
@@ -0,0 +1,9 @@
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class ExampleMod {
+    public void init() {
+        System.out.println("Hello world from my example mod!");
+
+    }
+}
